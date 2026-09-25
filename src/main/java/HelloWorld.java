d4e28e2b5662508d986bcb4bd285d7e98ccd84a6
Farhana0915 <2022wb86829@wilp.bits-pilani.ac.in>
2026-09-25 13:18:21 +0530

    Fix formatting in HelloWorld.java main method

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) { 
         System.out.println("Hello from 2022wb86829 - Continuous Integration!");
     }
 }
